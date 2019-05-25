4c09364392654cfe24a0f9197d1afc9b217097e3
zhou <623109799@qq.com>
2019-05-25 21:50:48 +0800

    refactor

--- src/main/java/cn/jely/cd/FizzBuzz.java
+++ src/main/java/cn/jely/cd/FizzBuzz.java
@@ -3,12 +3,16 @@ package cn.jely.cd;
 public class FizzBuzz {
     public String convert(int i) {
         String result = "";
-        if(i%3==0||String.valueOf(i).contains("3")) result +="Fizz";
-        if(i%5==0||String.valueOf(i).contains("5")) result += "Buzz";
+        if(i%3==0|| isContains(i, "3")) result +="Fizz";
+        if(i%5==0|| isContains(i, "5")) result += "Buzz";
         if (result.isEmpty()) {
             return result + i;
         } else {
             return result;
         }
+    }
+
+    private boolean isContains(int i, String s) {
+        return String.valueOf(i).contains(s);
     }
 }
