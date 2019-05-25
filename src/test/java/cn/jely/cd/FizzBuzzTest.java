264e4f78f4f541bb1593555bacc142532dba65f7
zhou <623109799@qq.com>
2019-05-25 21:28:31 +0800

    refactor test

--- src/test/java/cn/jely/cd/FizzBuzzTest.java
+++ src/test/java/cn/jely/cd/FizzBuzzTest.java
@@ -1,18 +1,17 @@
 package cn.jely.cd;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class FizzBuzzTest {
-    @Test
-    public void when_1_then_str_1() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.convert(1);
-        Assert.assertEquals(result, "1");
+    @DataProvider
+    public Object[][] param() {
+        return new Object[][]{{1, "1"}, {3, "Fizz"}};
     }
     @Test
-    public void when_3_then_str_fizz() {
+    public void fizzBuzzTest(int i,String expected) {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.convert(3);
-        Assert.assertEquals(result,"Fizz");
+        String result = fizzBuzz.convert(i);
+        Assert.assertEquals(result, expected);
     }
 }
