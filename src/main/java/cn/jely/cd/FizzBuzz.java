f30cd380715f248d597fca226bf03ecf3737d2a6
zhou <623109799@qq.com>
2019-05-25 21:45:36 +0800

    number contains 5 is fizz too ok

--- src/main/java/cn/jely/cd/FizzBuzz.java
+++ src/main/java/cn/jely/cd/FizzBuzz.java
@@ -3,7 +3,7 @@ package cn.jely.cd;
 public class FizzBuzz {
     public String convert(int i) {
         String result = "";
-        if(i%3==0) result +="Fizz";
+        if(i%3==0||String.valueOf(i).contains("3")) result +="Fizz";
         if(i%5==0) result += "Buzz";
         if (result.isEmpty()) {
             return result + i;
