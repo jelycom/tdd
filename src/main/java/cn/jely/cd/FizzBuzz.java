33256a5a58e4ab3120435ed23cfe74656e4ea69d
zhou <623109799@qq.com>
2019-05-25 21:35:45 +0800

    fizzbuzz test ok

--- src/main/java/cn/jely/cd/FizzBuzz.java
+++ src/main/java/cn/jely/cd/FizzBuzz.java
@@ -2,8 +2,13 @@ package cn.jely.cd;
 
 public class FizzBuzz {
     public String convert(int i) {
-        if(i%3==0) return "Fizz";
-        if(i%5==0) return "Buzz";
-        return "" + i;
+        String result = "";
+        if(i%3==0) result +="Fizz";
+        if(i%5==0) result += "Buzz";
+        if (result.isEmpty()) {
+            return result + i;
+        } else {
+            return result;
+        }
     }
 }
